14ecb4e9b3c2a4cd3d007c1e7fb4d51c79b88ddb
mehak <mehakkanyal@gmail.com>
2025-02-26 02:00:09 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className='text-3xl bg-orange-600 text-center'> hello g ,kese ho saare?</h1>
+    </>
+  )
+}
+
+export default App
